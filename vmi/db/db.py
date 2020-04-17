8ad9c3cd76fd68b1f1923731e8612f865c853880
ncteisen <ncteisen@google.com>
2020-04-17 09:37:03 -0700

    trigger scrape for shows missing imdb_id per episode

--- vmi/db/db.py
+++ vmi/db/db.py
@@ -19,13 +19,14 @@ class DbClient:
 		logging.info("Got handle for show %s!" % show_metadata.title)
 		if (self.pickler.has(show_metadata)):
 			logging.info("Show %s was found in the pickle DB!" % show_metadata.title)
-			return self.pickler.get(show_metadata)
-		else:
-			logging.info("Scraping data for show %s..." % show_metadata.title)
-			show = self.net.get_show(show_metadata)
-			logging.info("Done scraping data for show %s!" % show_metadata.title)
-			self.pickler.put(show)
-			return show
+			show = self.pickler.get(show_metadata)
+			# TODO(ncteisen): remove this once all pickles have new field imdb_id
+			if (hasattr(show.season_list[0].episode_list[0], "imdb_id")): return show
+		logging.info("Scraping data for show %s..." % show_metadata.title)
+		show = self.net.get_show(show_metadata)
+		logging.info("Done scraping data for show %s!" % show_metadata.title)
+		self.pickler.put(show)
+		return show
 
 # module testing only
 if __name__ == "__main__":
